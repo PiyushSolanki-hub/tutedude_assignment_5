ed4b39dde7512dc616fc712e9ff7e3f1d3da2c79
Piyush Solanki <piyushsolanki2521@gmail.com>
2025-11-26 19:51:40 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,8 @@
+students = {"Alice": 85, "Bob": 92, "Charlie": 78}
+
+name = input("Enter the student's name: ")
+
+if name in students:
+    print(f"{name}'s marks: {students[name]}")
+else:
+    print("Student not found.")
